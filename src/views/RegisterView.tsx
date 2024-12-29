b85c6792a8c51f07d5d55059b345c82cd21f2ca3
ErickGBR <erickburgos1519@gmail.com>
2024-12-29 15:39:31 -0600

    feat: add validation errors component with react-hook-form

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,14 +1,25 @@
 
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../components/ErrorMessage";
 
 export default function RegisterView() {
+
+    const { register, watch, handleSubmit, formState: { errors } } = useForm();
+
+    console.log(errors);
+
+    const handleRegister = async (data: any) => {
+        console.log("desde handle register");
+    }
+
     return (
         <div>
             <h1 className="text-4xl text-white font-bold">RegisterView</h1>
 
 
             <form
-                onSubmit={() => { }}
+                onSubmit={ handleSubmit(handleRegister) }
                 className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
             >
                 <div className="grid grid-cols-1 space-y-3">
@@ -18,7 +29,14 @@ export default function RegisterView() {
                         type="text"
                         placeholder="Your name"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name',
+                            {
+                                required: "Name is mandatory"
+                            }
+                        )
+                        }
                     />
+                    {errors.name &&  <ErrorMessage> { errors.name.message } </ErrorMessage>}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
@@ -27,7 +45,11 @@ export default function RegisterView() {
                         type="email"
                         placeholder="Email of register"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email',
+                            { required: "email is mandatory " }
+                        )}
                     />
+                    {errors.email &&  <ErrorMessage> { errors.email.message } </ErrorMessage>}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
@@ -36,7 +58,11 @@ export default function RegisterView() {
                         type="text"
                         placeholder="User name: not spaces, no special characters"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle',
+                            { required: "handle is mandatory " }
+                        )}
                     />
+                    {errors.handle &&  <ErrorMessage> { errors.handle.message } </ErrorMessage>}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
@@ -45,17 +71,25 @@ export default function RegisterView() {
                         type="password"
                         placeholder="Password of register"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password',
+                            { required: "password is mandatory " }
+                        )}
                     />
+                    {errors.password &&  <ErrorMessage> { errors.password.message } </ErrorMessage>}
                 </div>
 
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repeat Password</label>
                     <input
-                        id="password"
+                        id="password_confirmation"
                         type="password"
                         placeholder="Repeat password"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password_confirmation',
+                            { required: "password_confirmation is mandatory " }
+                        )}
                     />
+                    {errors.password_confirmation &&  <ErrorMessage> { errors.password_confirmation.message } </ErrorMessage>}
                 </div>
 
                 <input
