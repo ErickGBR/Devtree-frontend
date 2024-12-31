f93945902846579add39451c4dfe9b3e2019f399
ErickGBR <erickburgos1519@gmail.com>
2024-12-31 02:08:06 -0600

    feat: add login form with validations and default fields

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,15 @@
-export type IUser = {
+export type User = {
     handle: string;
     name: string;
     email: string;
     password: string;
 }
 
-export type Registerform = Pick <IUser, 'handle' | 'name' | 'email' > & {
+export type Registerform = Pick <User, 'handle' | 'name' | 'email' > & {
     password: string;
     password_confirmation: string;
+}
+
+export type LoginForm = Pick <User, 'email' > & {
+    password: string;
 }
