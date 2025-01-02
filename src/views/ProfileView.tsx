ab36b5e33579194e6b3f5ffd0d8d5b5dd326e521
ErickGBR <erickburgos1519@gmail.com>
2025-01-02 00:12:52 -0600

    feat: create form user information

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,8 +1,51 @@
-
 export default function ProfileView() {
+
     return (
-        <div>
-            <h1>ProfileView</h1>
-        </div>
+        <form 
+            className="bg-white p-10 rounded-lg space-y-5"
+            onSubmit={() => {}}
+        >
+            <legend className="text-2xl text-slate-800 text-center">Información</legend>
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Handle:</label>
+                <input
+                    type="text"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="handle o Nombre de Usuario"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="description"
+                > Description :</label>
+                <textarea
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="Tu Descripción"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Image:</label>
+                <input
+                    id="image"
+                    type="file"
+                    name="handle"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    accept="image/*"
+                    onChange={ () => {} }
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                value='Save Changes'
+            />
+        </form>
     )
 }
