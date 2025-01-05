a3b28d2cfca0af16b8a85c3f76f19901de282e22
ErickGBR <erickburgos1519@gmail.com>
2025-01-04 23:15:22 -0600

    feat: add react query global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './index.css'
-import App from './router'
+import Router from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <Router />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
