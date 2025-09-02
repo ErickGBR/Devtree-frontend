14af517a1eafa5660ab74098c09ef3752a8ca29c
Erick Burgos <eburgosrivas1997@gmail.com>
2025-09-01 21:16:42 -0600

    feat: add home component screen styles

--- src/components/Nav/HomeNavigation.tsx
+++ src/components/Nav/HomeNavigation.tsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 function HomeNavigation() {
     return (
         <>
-            <Link 
-            className="text-white p-2 uppercase font-black text-xs cursor-pointer"
-            to="/auth/login">
-            Login
+            <Link
+                className="text-white p-2 uppercase font-black text-xs cursor-pointer"
+                to="/auth/login">
+                Login
             </Link>
             <Link
-            className="bg-lime-500 text-slate-900 p-2 uppercase font-black text-xs cursor-pointer rounded-lg"
-            to="/auth/register">
-            Register
+                className="bg-lime-500 text-slate-900 p-2 uppercase font-black text-xs cursor-pointer rounded-lg"
+                to="/auth/register">
+                Register
             </Link>
         </>
     );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -2,10 +2,24 @@ import Header from "../components/Header";
 
 function HomeView() {
     return (
-        <div>
+        <>
             <Header />
-            {/* Home view content goes here */}
-        </div>
+            <main className="bg-gray-100 py-0 main-h-screen">
+                <div className="max-w-5xl mx-auto mt-10">
+                    <div className="lg:w-1/2 px-10 lg:px-0 space-y-6">
+                        <h1 className="text-6xl font-black">
+                            All you <span className="text-cyan-400">Social Networsks </span>
+                            in one place
+                        </h1>
+                        <p className="text-slate-800 text-xl">
+                            Join thousands of users who are using DevTree to share their social networks, share you
+                            profile of Tiktok, Instagram, Twitter, Linkedin, Facebook, Youtube, Github and more.
+                        </p>
+                    </div>
+                </div>
+            </main>
+
+        </>
     );
 }
 
