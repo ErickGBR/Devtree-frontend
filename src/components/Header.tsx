38ad04f1050a26a6026e707b3b84c8460ba6f86d
Erick Burgos <eburgosrivas1997@gmail.com>
2025-09-01 20:59:38 -0600

    feat: add logo ccomponent and navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import Logo from "./logo";
 import { useLocation } from "react-router-dom";
 import AdminNavigation from "./Nav/AdminNavigation";
 import HomeNavigation from "./Nav/HomeNavigation";
@@ -10,9 +10,7 @@ function Header() {
         <header className="bg-slate-800 py-5">
             <div className="mx-auto max-w-5xl flex flex-col md:flex-row items-center md:justify-between">
                 <div className="w-full p-5 lg:p-0 md:w-1/3">
-                    <Link to="/">
-                        <img src="/logo.svg" className="w-full block" />
-                    </Link>
+                    <Logo />
                 </div>
                 <nav className="md:w-1/3 md:flex md:justify-end">
                     {location.pathname == "/" ? (
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -5,13 +5,14 @@
 */
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import Logo from "../components/logo";
 
 export default function AuthLayouts() {
     return (
         <>
             <div className="bg-slate-800 min-h-screen">
                 <div className="max-w-lg mx-auto pt-10 px-5">
-                    <img src="/logo.svg" alt="logotipo" />
+                    <Logo />
                     <div className="py-10">
                         <Outlet />
                     </div>
