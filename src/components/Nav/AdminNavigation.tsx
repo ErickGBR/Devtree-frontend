10139aa3ca36fe74f8ced4c73fdac9c2e3063cf7
Erick Burgos <eburgosrivas1997@gmail.com>
2025-09-28 10:38:23 -0600

    feat: add logout

--- src/components/Nav/AdminNavigation.tsx
+++ src/components/Nav/AdminNavigation.tsx
@@ -1,10 +1,23 @@
 
+import { useQueryClient } from "@tanstack/react-query";
+
 
 function AdminNavigation() {
+
+    const queryClient = useQueryClient();
+
+
+    const logout = async () => {
+        localStorage.removeItem("AUTH_TOKEN");
+        queryClient.invalidateQueries({ queryKey: ['user'] });
+        window.location.reload();
+    };
+
+
     return (
         <button
             className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-            onClick={() => { }}
+            onClick={logout}
         >
             Logout
         </button>
